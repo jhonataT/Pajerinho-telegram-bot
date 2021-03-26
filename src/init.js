2dacfdd0065a6be1e7c80203d58bd9af781d3ed5
Jhn <jhonata.tenorio2526@gmail.com>
2021-03-25 23:04:34 -0300

    create class SaveGroupData

---
+++ src/init.js
@@ -0,0 +1,11 @@
+class InitGroup {
+    constructor(groupData, userData){
+        this.data = groupData;
+        this.data = userData;
+    }
+
+    register(){
+        // Verify if user is admin
+        // Write group id in a json file
+    }
+}
