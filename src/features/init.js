18f5c63b42e90e71ed0d19cfa47d96c681009b49
Jhn <jhonata.tenorio2526@gmail.com>
2021-03-31 23:39:47 -0300

    try debug

--- src/features/init.js
+++ src/features/init.js
@@ -1,9 +1,9 @@
 const { group } = require('console');
 const fs = require('fs');
 
-class SaveGroupData {
-    constructor(groupData, groupId){
-        this.data = groupData;
+class SavedataFile {
+    constructor(dataFile, groupId){
+        this.data = dataFile;
         this.groupId = groupId;
     }
 
@@ -11,30 +11,30 @@ class SaveGroupData {
         // Write group id in a JSON file
         let isErr = false;
         console.log("BEFORE");
-        let groupData = fs.readFileSync('Database/Groups.json');
-        groupData = groupData.toString();
+        let dataFile = fs.readFileSync('DataBase/Groups.json');
+        dataFile = dataFile.toString();
         console.log("AFTER");
         
 
-        console.log(groupData);
-        if(groupData != ""){
-            for(let i = 0; i < groupData.length; i++){
-                groupData = groupData.replace('"', ""); 
+        console.log(dataFile);
+        if(dataFile != ""){
+            for(let i = 0; i < dataFile.length; i++){
+                dataFile = dataFile.replace('"', ""); 
             }
-            groupData = groupData.split(',');
-            console.table(groupData);
+            dataFile = dataFile.split(',');
+            console.table(dataFile);
         }
         
-        for(let i = 0; i < groupData.length; i += 2){
-            if(groupData[i] == this.groupId){
+        for(let i = 0; i < dataFile.length; i += 2){
+            if(dataFile[i] == this.groupId){
                 return `Esse grupo já está cadastrado.`;
             }
         }
 
-        groupData = groupData.toString() + this.data;
-        console.log(groupData);
+        dataFile = dataFile.toString() + this.data;
+        console.log(dataFile);
 
-        const err = fs.writeFileSync('Database/Groups.json', JSON.stringify(groupData, null, 4));
+        const err = fs.writeFileSync('Database/Groups.json', JSON.stringify(dataFile, null, 4));
         if(err) isErr = true;
         else console.log('Sucess writeFile');
 
@@ -43,4 +43,4 @@ class SaveGroupData {
     }
 }
 
-module.exports = SaveGroupData;
+module.exports = SavedataFile;
